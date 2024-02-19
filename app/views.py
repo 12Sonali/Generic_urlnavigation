2128e10041bec5db601624509dfbb89ceba1b5a3
Sonali Misra <sonalimisra2001@gmail.com>
2024-02-19 11:16:05 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def tupuri(request):
+    return render(request,'tupuri.html')
+
+def sona(request):
+    return render(request,'sona.html')
+
+
+def sonali(request):
+    return render(request,'sonali.html')
